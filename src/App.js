e0dd1340368947f3c1f65634036caff42701d393
RatchanonPang <pang20144@hotmail.com>
2018-01-19 16:07:39 +0700

    add rounter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import _ from 'lodash';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 const students =[
   {id: 34,name: 'Pang', score: 50},
   {id: 18,name: 'Jason', score: 27},
   {id: 20,name: 'Tim', score: 22},
 ]
+
+const Home = () => (<div>Home</div>)
+const Students = () => (
+  <div>
+    students
+  </div>
+)
 
 const StudentLine = (props) =>(
   <div>{props.id} {props.name} = {props.score}</div>
@@ -18,7 +30,12 @@ class App extends Component {
     return (
       <div className="App">
         {
-          _.map(students,s => (<StudentLine {...s}key={s.id}/>))
+          <Router>
+          <div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/students" component={Students}/> 
+          </div>       
+        </Router>
         }
       </div>
     );
@@ -26,3 +43,5 @@ class App extends Component {
 }
 
 export default App;
+
+
