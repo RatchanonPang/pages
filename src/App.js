6c61753243ba9276aaed304123b64dec91534dcf
RatchanonPang <pang20144@hotmail.com>
2018-01-19 16:00:54 +0700

    เพิ่ม น.ร. , แสดงคะแนน

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BasicExample from './BasicExample'
+import _ from 'lodash';
+
+const students =[
+  {id: 34,name: 'Pang', score: 50},
+  {id: 18,name: 'Jason', score: 27},
+  {id: 20,name: 'Tim', score: 22},
+]
+
+const StudentLine = (props) =>(
+  <div>{props.id} {props.name} = {props.score}</div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BasicExample/>
+        {
+          _.map(students,s => (<StudentLine {...s}key={s.id}/>))
+        }
       </div>
     );
   }
